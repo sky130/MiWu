a75d498bf64a32926e53186731099e8a4476aa12
honoz <honoz@qq.com>
2023-08-18 16:33:25 +0800

    更新UI

--- app/src/main/java/io/github/sky130/miwu/widget/MainTitleBar.kt
+++ app/src/main/java/io/github/sky130/miwu/widget/MainTitleBar.kt
@@ -1,35 +1,34 @@
-package io.github.sky130.miwu.widget;
-import android.content.Context;
-import android.content.res.TypedArray;
-import android.util.AttributeSet;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import io.github.sky130.miwu.R;
+package io.github.sky130.miwu.widget
 
-public class MainTitleBar extends LinearLayout {
-    public TextView a;
-    public String b;
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+import android.widget.TextView
+import io.github.sky130.miwu.R
 
-    public MainTitleBar(Context var1, AttributeSet var2) {
-        super(var1, var2);
-        TypedArray var10009 = var1.obtainStyledAttributes(var2, R.styleable.MiTitleBar, 0, 0);
-        this.b = var10009.getString(R.styleable.MiTitleBar_title);
-        var10009.recycle();
-        this.setGravity(16);
-        View.inflate(var1, R.layout.main_title_bar, this);
-        this.setPadding(var1.getResources().getDimensionPixelSize(R.dimen.content_horizontal_distance), 0, 0, 0);
-        this.a = this.findViewById(R.id.textView);
-        this.a.setText(this.b);
-        this.a.setSelected(true);
-        this.a.setMarqueeRepeatLimit(-1);
+class MainTitleBar(var1: Context, var2: AttributeSet?) : LinearLayout(var1, var2) {
+    var titleTextView: TextView
+    var b: String?
+
+    init {
+        val var10009 = var1.obtainStyledAttributes(var2, R.styleable.MiTitleBar, 0, 0)
+        b = var10009.getString(R.styleable.MiTitleBar_title)
+        var10009.recycle()
+        this.gravity = 16
+        inflate(var1, R.layout.main_title_bar, this)
+        setPadding(
+            var1.resources.getDimensionPixelSize(R.dimen.content_horizontal_distance),
+            0,
+            0,
+            0
+        )
+        titleTextView = findViewById(R.id.textView)
+        titleTextView.text = b
+        titleTextView.isSelected = true
+        titleTextView.marqueeRepeatLimit = -1
     }
 
-    public void setTitle(String var1) {
-        this.a.setText(var1);
-    }
-
-    public TextView getTitleTextView() {
-        return this.a;
+    fun setTitle(var1: String?) {
+        titleTextView.text = var1
     }
 }

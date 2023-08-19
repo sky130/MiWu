ec2a17d3792721231999bed366a3dc76a559127c
Sky233ml <sky233@sky233.ml>
2023-08-19 20:47:52 +0800

    完善设置界面的缓存机制

--- app/src/main/java/io/github/sky130/miwu/ui/framgent/SettingsFragment.kt
+++ app/src/main/java/io/github/sky130/miwu/ui/framgent/SettingsFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import io.github.sky130.miwu.MainApplication.Companion.loginMsg
 import io.github.sky130.miwu.R
 import io.github.sky130.miwu.databinding.FragmentMainSettingsBinding
+import io.github.sky130.miwu.logic.dao.UserDAO
 import io.github.sky130.miwu.logic.network.miot.UserService
 import io.github.sky130.miwu.util.GlideUtils
 import java.util.concurrent.Executors
@@ -14,31 +15,27 @@ import java.util.concurrent.Executors
 class SettingsFragment : BaseFragment() {
     private lateinit var binding: FragmentMainSettingsBinding
     private val executor = Executors.newSingleThreadExecutor()
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        savedInstanceState: Bundle?,
+    ): View {
         binding = FragmentMainSettingsBinding.inflate(layoutInflater)
         val avatar = binding.avatarImage
         val uid = binding.uidText
         val name = binding.nicknameText
         executor.execute {
-            val UserInfo = UserService.getUserInfo(loginMsg.userId)
+            val info = UserDAO.getLocalUserInfo()
+            val userInfo = if (info.uid.isEmpty()) UserDAO.getLatestUserInfo() else info
             // 更新UI需要切换到主线程
-            activity?.runOnUiThread {
-                if (UserInfo != null) {
-                    if (UserInfo.avatar.isNotEmpty()){
-                        GlideUtils.loadImg(UserInfo.avatar, avatar)
-                    } else {
-                        avatar.setImageResource(R.drawable.mi_icon_small)
-                    }
-                    uid.text = UserInfo.uid
-                    name.text = UserInfo.nickname
+            runOnUiThread {
+                if (userInfo.avatar.isNotEmpty()) {
+                    GlideUtils.loadImg(userInfo.avatar, avatar)
+                } else {
+                    avatar.setImageResource(R.drawable.mi_icon_small)
                 }
+                uid.text = userInfo.uid
+                name.text = userInfo.nickname
             }
         }
         return binding.root

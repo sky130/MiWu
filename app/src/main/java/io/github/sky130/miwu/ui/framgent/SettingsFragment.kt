4867982d0e92f32bb1b46e5a6b59a71b21be95ed
honoz <honoz@qq.com>
2023-08-19 17:28:34 +0800

    实现逻辑

--- app/src/main/java/io/github/sky130/miwu/ui/framgent/SettingsFragment.kt
+++ app/src/main/java/io/github/sky130/miwu/ui/framgent/SettingsFragment.kt
@@ -1,41 +1,47 @@
 package io.github.sky130.miwu.ui.framgent
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import io.github.sky130.miwu.MainApplication.Companion.loginMsg
 import io.github.sky130.miwu.R
-import io.github.sky130.miwu.logic.network.MiotService
+import io.github.sky130.miwu.databinding.FragmentMainSettingsBinding
 import io.github.sky130.miwu.logic.network.miot.UserService
 import io.github.sky130.miwu.util.GlideUtils
 import java.util.concurrent.Executors
 
 class SettingsFragment : BaseFragment() {
+    private lateinit var binding: FragmentMainSettingsBinding
     private val executor = Executors.newSingleThreadExecutor()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-            executor.execute {
-                val UserInfo = UserService.getUserInfo(loginMsg.userId)
-                // 更新UI需要切换到主线程
-                activity?.runOnUiThread {
-                    if (UserInfo != null) {
-                        Log.d("uid", UserInfo.uid)
-                        Log.d("avatar", UserInfo.avatar)
-                        Log.d("nickname", UserInfo.nickname)
-                    }
-                }
-            }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_settings, container, false)
+        binding = FragmentMainSettingsBinding.inflate(layoutInflater)
+        val avatar = binding.avatarImage
+        val uid = binding.uidText
+        val name = binding.nicknameText
+        executor.execute {
+            val UserInfo = UserService.getUserInfo(loginMsg.userId)
+            // 更新UI需要切换到主线程
+            activity?.runOnUiThread {
+                if (UserInfo != null) {
+                    if (UserInfo.avatar.isNotEmpty()){
+                        GlideUtils.loadImg(UserInfo.avatar, avatar)
+                    } else {
+                        avatar.setImageResource(R.drawable.mi_icon_small)
+                    }
+                    uid.text = UserInfo.uid
+                    name.text = UserInfo.nickname
+                }
+            }
+        }
+        return binding.root
     }
 
 }

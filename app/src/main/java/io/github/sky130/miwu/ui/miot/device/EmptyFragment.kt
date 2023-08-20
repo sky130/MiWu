76e5f2dd288510f143dd4ed5e633b1025ab81498
honoz <honoz@qq.com>
2023-08-20 10:46:42 +0800

    修改数据显示

--- app/src/main/java/io/github/sky130/miwu/ui/miot/device/EmptyFragment.kt
+++ app/src/main/java/io/github/sky130/miwu/ui/miot/device/EmptyFragment.kt
@@ -4,8 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import io.github.sky130.miwu.R
 import io.github.sky130.miwu.databinding.DeviceEmptyBinding
-import io.github.sky130.miwu.logic.network.MiotService
+import io.github.sky130.miwu.logic.dao.HomeDAO
 import io.github.sky130.miwu.ui.miot.BaseFragment
 import io.github.sky130.miwu.util.GlideUtils
 import java.util.concurrent.Executors
@@ -18,21 +19,24 @@ class EmptyFragment : BaseFragment() {
         inflater: LayoutInflater, container: ViewGroup?, bundle: Bundle?,
     ): View {
         binding = DeviceEmptyBinding.inflate(inflater)
-        loadImageAsync()
-        return binding.root
-    }
-
-    fun loadImageAsync() {
         executor.execute {
-            val url = MiotService.getModelIconUrl(getModel())
-            // 更新UI需要切换到主线程
-            runOnUiThread {
-                if (url != null) {
-                    GlideUtils.loadImg(url, binding.deviceImage)
+            val home = HomeDAO.getHome(HomeDAO.getHomeIndex()) // 获取家庭对象
+            var url = ""
+            var isOnline = false
+            home?.deviceList?.forEach { device ->
+                if (device.model == getModel()) {
+                    url = device.iconUrl
+                    isOnline = device.isOnline
                 }
             }
+            // 更新UI需要切换到主线程
+            runOnUiThread {
+                if (url.isNotEmpty()) GlideUtils.loadImg(url, binding.deviceImage)
+                if (isOnline) binding.deviceOnline.text =
+                    getString(R.string.device_online) else binding.deviceOnline.text =
+                    getString(R.string.device_offline)
+            }
         }
+        return binding.root
     }
-
-
 }

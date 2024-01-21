d9f5bc25bf86a0a6d79e954bf4be12f164eb1ac2
Sky233ml <sky233@sky233.ml>
2024-01-21 12:58:04 +0800

    优化选择家庭界面

--- app/src/main/java/com/github/miwu/viewModel/HomeViewModel.kt
+++ app/src/main/java/com/github/miwu/viewModel/HomeViewModel.kt
@@ -1,8 +1,11 @@
 package com.github.miwu.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.github.miwu.R
 import com.github.miwu.logic.preferences.AppPreferences
 import com.github.miwu.logic.repository.AppRepository
+import kndroidx.extension.log
+import kndroidx.extension.string
 import miot.kotlin.model.miot.MiotHomes
 
 class HomeViewModel : ViewModel() {
@@ -13,4 +16,18 @@ class HomeViewModel : ViewModel() {
 
     fun isHome(item: MiotHomes.Result.Home) = item.id.toLong() == AppPreferences.homeId
 
+
+    fun getDesc(item: MiotHomes.Result.Home): String {
+        return if (item.shareFlag == 0) { // 本地家庭
+            var deviceSize = item.dids.size
+            for (i in item.rooms) {
+                deviceSize += i.dids.size
+            }
+            R.string.home_desc.string.format(item.rooms.size, deviceSize)
+        } else { // 共享家庭
+            R.string.home_desc_share.string
+        }.apply {
+            log.d()
+        }
+    }
 }

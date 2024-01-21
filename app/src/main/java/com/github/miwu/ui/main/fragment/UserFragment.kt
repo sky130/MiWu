c21b775161b9388113b11db7d4db4c120bd348f5
Sky233ml <sky233@sky233.ml>
2024-01-21 13:25:12 +0800

    优化用户界面并支持退出登录

--- app/src/main/java/com/github/miwu/ui/main/fragment/UserFragment.kt
+++ app/src/main/java/com/github/miwu/ui/main/fragment/UserFragment.kt
@@ -2,13 +2,17 @@ package com.github.miwu.ui.main.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.github.miwu.MainApplication
 import com.github.miwu.databinding.FragmentMainDeviceBinding
 import com.github.miwu.databinding.FragmentMainSettingsBinding
+import com.github.miwu.logic.preferences.AppPreferences
 import com.github.miwu.ui.home.HomeActivity
+import com.github.miwu.ui.login.LoginActivity
 import com.github.miwu.viewmodel.MainViewModel
 import kndroidx.activity.ViewActivityX
 import kndroidx.extension.start
 import kndroidx.fragment.ViewFragmentX
+import miot.kotlin.Miot
 
 class UserFragment : ViewFragmentX<FragmentMainSettingsBinding, MainViewModel>() {
     override fun init() {
@@ -20,7 +24,19 @@ class UserFragment : ViewFragmentX<FragmentMainSettingsBinding, MainViewModel>()
         binding.root.requestFocus()
     }
 
-    fun startHomeActivity(){
+    fun startHomeActivity() {
         requireContext().start<HomeActivity>()
+    }
+
+    fun logout() {
+        AppPreferences.apply {
+            userId = ""
+            securityToken = ""
+            homeId = 0
+            homeUid = 0
+            serviceToken = ""
+        }
+        requireActivity().start<LoginActivity>()
+        requireActivity().finish()
     }
 }

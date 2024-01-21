9499af4ea557373c08cddfd7ad457edfea91a226
Sky233ml <sky233@sky233.ml>
2024-01-21 17:26:57 +0800

    修改版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.github.miwu"
         minSdk = 26 // 来个人把Base64换掉就能改成21了
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "2.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildFeatures {

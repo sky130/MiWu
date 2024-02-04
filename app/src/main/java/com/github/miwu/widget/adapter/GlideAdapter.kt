87f1958190d625e98652886577a5a0490b2c39ed
Sky233ml <sky233@sky233.ml>
2024-02-04 22:18:48 +0800

    修复加载图标概率闪退

--- app/src/main/java/com/github/miwu/widget/adapter/GlideAdapter.kt
+++ app/src/main/java/com/github/miwu/widget/adapter/GlideAdapter.kt
@@ -21,91 +21,79 @@ import miot.kotlin.model.miot.MiotDevices
 import miot.kotlin.model.miot.MiotScenes
 import org.json.JSONObject
 
+@get:Synchronized
 val iconMap = ArrayMap<String, String>()
 
 @BindingAdapter(value = ["device", "fragment"])
 fun miotIcon(imageView: ImageView, device: MiotDevices.Result.Device, fragment: DeviceFragment) {
-    Glide.with(imageView.context)
-        .load(R.drawable.ic_miwu_placeholder)
-        .into(imageView)
+    Glide.with(imageView.context).load(R.drawable.ic_miwu_placeholder).into(imageView)
     val url = iconMap[device.model]
     if (url == null) {
         fragment.viewModel.viewModelScope.launch(Dispatchers.IO) {
-            device.getIconUrl()?.also {
-                iconMap[device.model] = it
+            device.getIconUrl().apply {
                 withContext(Dispatchers.Main) {
-                    loadImageUrl(imageView, it)
-                }
-            }.let {
-                if (it == null) {
-                    iconMap[device.model] = ""
+                    if (this@apply == null) {
+                        iconMap[device.model] = ""
+                    } else {
+                        iconMap[device.model] = this@apply
+                        loadImageUrl(imageView, this@apply)
+                    }
                 }
             }
         }
-    } else {
-        if (url.isNotEmpty())
-            loadImageUrl(imageView, url)
+    } else if (url.isNotEmpty()) {
+        loadImageUrl(imageView, url)
     }
 }
 
 fun ImageView.loadMiotIcon(device: MiwuDevice, viewModel: ViewModel) {
-    Glide.with(context)
-        .load(R.drawable.ic_miwu_placeholder)
-        .into(this)
+    Glide.with(context).load(R.drawable.ic_miwu_placeholder).into(this)
     val url = iconMap[device.model]
     if (url == null) {
         viewModel.viewModelScope.launch(Dispatchers.IO) {
-            device.getIconUrl()?.also {
-                iconMap[device.model] = it
+            device.getIconUrl().apply {
                 withContext(Dispatchers.Main) {
-                    loadImageUrl(this@loadMiotIcon, it)
-                }
-            }.let {
-                if (it == null) {
-                    iconMap[device.model] = ""
+                    if (this@apply == null) {
+                        iconMap[device.model] = ""
+                    } else {
+                        iconMap[device.model] = this@apply
+                        loadImageUrl(this@loadMiotIcon, this@apply)
+                    }
                 }
             }
         }
-    } else {
-        if (url.isNotEmpty())
-            loadImageUrl(this, url)
+    } else if (url.isNotEmpty()) {
+        loadImageUrl(this, url)
     }
 }
 
 @BindingAdapter(value = ["url"])
 fun loadImg(imageView: ImageView, url: String?) {
-    if (url != null)
-        loadImageUrl(imageView, url)
+    if (url != null) loadImageUrl(imageView, url)
 }
 
 @BindingAdapter(value = ["scene"])
 fun loadImg(imageView: ImageView, scene: MiotScenes.Result.Scene) {
-    if (scene.icon.isNotEmpty())
-        loadImageUrl(imageView, scene.icon)
+    if (scene.icon.isNotEmpty()) loadImageUrl(imageView, scene.icon)
     else {
         loadImageRes(imageView, R.drawable.ic_miot_scene)
     }
 }
 
 fun loadImageUrl(imageView: ImageView, url: String) {
-    Glide.with(imageView.context)
-        .load(url)
-        .placeholder(R.drawable.ic_miwu_placeholder)
-        .error(R.drawable.ic_miwu_placeholder)
-        .into(imageView)
+    Glide.with(imageView.context).load(url).placeholder(R.drawable.ic_miwu_placeholder)
+        .error(R.drawable.ic_miwu_placeholder).into(imageView)
 }
 
 fun loadImageRes(imageView: ImageView, @DrawableRes res: Int) {
-    Glide.with(imageView.context)
-        .load(res)
-        .placeholder(R.drawable.ic_miwu_placeholder)
-        .error(R.drawable.ic_miwu_placeholder)
-        .into(imageView)
+    Glide.with(imageView.context).load(res).placeholder(R.drawable.ic_miwu_placeholder)
+        .error(R.drawable.ic_miwu_placeholder).into(imageView)
 }
 
 suspend fun MiwuDevice.getIconUrl(): String? = withContext(Dispatchers.IO) {
     try {
-        val url = "https://home.mi.com/cgi-op/api/v1/baike/v2/product?model=${this@getIconUrl.model}"
+        val url =
+            "https://home.mi.com/cgi-op/api/v1/baike/v2/product?model=${this@getIconUrl.model}"
         val json = MiotManager.get(url)
         val jsonObject = JSONObject(json)
         if (jsonObject.getInt("code") != 0) return@withContext null

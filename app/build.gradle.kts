494e0d7c09e38604ac0d5a26bb8d3a93561cc8fd
Sky233ml <sky233@sky233.ml>
2024-02-05 23:54:20 +0800

    修改版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.github.miwu"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "2.0.0"
+        versionCode = 3
+        versionName = "2.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildFeatures {

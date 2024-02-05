b60d3670f15664bf2db875d98429ab4d38b91ed1
Sky233ml <sky233@sky233.ml>
2024-02-05 21:50:06 +0800

    完善单位

--- app/src/main/java/com/github/miwu/miot/utils/Unit.kt
+++ app/src/main/java/com/github/miwu/miot/utils/Unit.kt
@@ -3,5 +3,11 @@ package com.github.miwu.miot.utils
 fun getUnitString(unit: String) = when (unit) {
     "celsius" -> "°C"
     "percentage" -> "%"
+    "seconds" -> "s"
+    "kelvin" -> "K"
+    "hours" -> "h"
+    "minutes" -> "min"
+    "pascal" -> "Pa"
+    "arcdegrees" -> "Rad"
     else -> unit
 }

3afd320d34abb83511ea67d5f2b1b1adcbdb26fb
Sky233ml <sky233@sky233.ml>
2024-02-08 18:01:45 +0800

    尝试优化退出登录（

--- app/src/main/java/com/github/miwu/ui/main/fragment/UserFragment.kt
+++ app/src/main/java/com/github/miwu/ui/main/fragment/UserFragment.kt
@@ -1,6 +1,8 @@
 package com.github.miwu.ui.main.fragment
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import com.github.miwu.MainApplication
 import com.github.miwu.databinding.FragmentMainDeviceBinding
@@ -13,10 +15,14 @@ import com.github.miwu.ui.smart.SmartActivity
 import com.github.miwu.viewmodel.MainViewModel
 import kndroidx.activity.ViewActivityX
 import kndroidx.extension.start
+import kndroidx.extension.toast
 import kndroidx.fragment.ViewFragmentX
 import miot.kotlin.Miot
+import kotlin.system.exitProcess
 
 class UserFragment : ViewFragmentX<FragmentMainSettingsBinding, MainViewModel>() {
+    private var exit = 0
+
     override fun init() {
         viewModel.loadInfo()
     }
@@ -30,6 +36,10 @@ class UserFragment : ViewFragmentX<FragmentMainSettingsBinding, MainViewModel>()
     }
 
     fun logout() {
+        if (exit == 0) {
+            exit++
+            return "再点击一次即可退出登录\n重进应用时即可登录".toast()
+        }
         AppPreferences.apply {
             userId = ""
             securityToken = ""
@@ -37,8 +47,8 @@ class UserFragment : ViewFragmentX<FragmentMainSettingsBinding, MainViewModel>()
             homeUid = 0
             serviceToken = ""
         }
-        requireActivity().start<LoginActivity>()
         requireActivity().finish()
+        Handler(Looper.getMainLooper()).postDelayed({exitProcess(0)},500)
     }
 
     fun startSmartActivity() {

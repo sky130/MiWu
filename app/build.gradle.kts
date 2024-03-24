ede6afdd1b571fc99808c4e075c74c50fdbd948c
Sky233ml <sky233@sky233.ml>
2024-03-24 10:41:54 +0800

    修改版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.github.miwu"
         minSdk = 26
         targetSdk = 34
-        versionCode = 5
-        versionName = "2.0.3"
+        versionCode = 6
+        versionName = "2.0.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ksp {
             arg("room.schemaLocation", "$projectDir/schemas")

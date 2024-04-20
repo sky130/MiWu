39474a2a80c6fd325169c14642918ce2f46145f6
Sky233ml <sky233@sky233.ml>
2024-04-20 15:02:46 +0800

    测试调整

--- miot-api/build.gradle.kts
+++ miot-api/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.fir.declarations.builder.buildTypeAlias
+
 plugins {
     kotlin("jvm")
     `maven-publish`
@@ -16,30 +18,4 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
     implementation("com.squareup.okio:okio:3.4.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-}
-
-afterEvaluate {
-    publishing {
-        publications {
-            register<MavenPublication>("release") {
-                groupId = "com.github.sky233.miwu"
-                artifactId = "miot"
-                version = latestGitTag()
-
-                afterEvaluate {
-                    from(components["release"])
-                }
-            }
-        }
-        repositories {
-            mavenLocal()
-        }
-    }
-}
-
-fun latestGitTag(): String {
-    val process = ProcessBuilder("git", "describe", "--tags", "--abbrev=0").start()
-    return process.inputStream.bufferedReader().use { bufferedReader ->
-        bufferedReader.readText().trim().replace("v", "").ifEmpty { "debug" }
-    }
 }

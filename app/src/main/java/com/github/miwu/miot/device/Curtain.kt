fbef0227442002ca46461a16e038c5b41832bdc1
Sky233ml <sky233@sky233.ml>
2024-05-04 23:28:04 +0800

    Features: 完善窗帘设备

--- app/src/main/java/com/github/miwu/miot/device/Curtain.kt
+++ app/src/main/java/com/github/miwu/miot/device/Curtain.kt
@@ -2,17 +2,23 @@ package com.github.miwu.miot.device
 
 import android.view.ViewGroup
 import com.github.miwu.miot.SpecAttClass
-import com.github.miwu.miot.SpecAttHelper
 import com.github.miwu.miot.manager.MiotDeviceManager
+import com.github.miwu.miot.SpecAttHelper
+import com.github.miwu.miot.quick.MiotBaseQuick
+import com.github.miwu.miot.widget.AirerBar
+import com.github.miwu.miot.widget.CurtainBar
+import com.github.miwu.miot.widget.StatusText
+import com.github.miwu.miot.widget.Switch
 import miot.kotlin.model.att.SpecAtt
 import miot.kotlin.model.miot.MiotDevices
 
 @SpecAttClass("curtain")
 class Curtain(device: MiotDevices.Result.Device, layout: ViewGroup?, manager: MiotDeviceManager?) :
     DeviceType(device, layout, manager), SpecAttHelper {
+    private val bar by lazy { createView<CurtainBar>() }
 
+    override fun getQuick(): MiotBaseQuick? = null
     override fun onLayout(att: SpecAtt) = forEachAtt(att)
-
 
     override fun onPropertyFound(
         siid: Int,
@@ -20,8 +26,18 @@ class Curtain(device: MiotDevices.Result.Device, layout: ViewGroup?, manager: Mi
         piid: Int,
         property: String,
         serviceDesc: String,
-        obj: SpecAtt.Service.Property
+        obj: SpecAtt.Service.Property,
     ) {
+        when (service to property) {
+            "curtain" to "motor-control" -> {
+                bar.properties.add(siid to obj)
+            }
+
+            "curtain" to "status" -> {
+                bar.properties.add(siid to obj)
+                createView<StatusText>(siid, piid, obj, index = 0)
+            }
+        }
     }
 
     override fun onActionFound(
@@ -30,8 +46,9 @@ class Curtain(device: MiotDevices.Result.Device, layout: ViewGroup?, manager: Mi
         aiid: Int,
         action: String,
         serviceDesc: String,
-        obj: SpecAtt.Service.Action
+        obj: SpecAtt.Service.Action,
     ) {
 
     }
+
 }
---
+++ app/src/main/java/com/github/miwu/miot/widget/CurtainBar.kt
@@ -0,0 +1,63 @@
+package com.github.miwu.miot.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import com.github.miwu.R
+import com.github.miwu.databinding.MiotWidgetAirerBarBinding as Binding
+import kndroidx.extension.compareTo
+import miot.kotlin.utils.parseUrn
+
+
+class CurtainBar(context: Context) : MiotBaseWidget<Binding>(context) {
+    private val modeList get() = getProperty("motor-control").valueList!!
+    private var status = 0
+        set(value) {
+            field = value
+            refreshMode(value)
+        }
+
+    private fun getModeDesc(value: Int) =
+        modeList.find { it.value == value }!!.description
+
+    override fun init() = binding.run {
+        val control = getPropertyWithSiid("motor-control")
+        up.setOnClickListener {
+            status = if (status != 1) {
+                1
+            } else {
+                0
+            }
+            putValue(status, control.first, control.second.iid)
+        }
+        down.setOnClickListener {
+            status = if (status != 2) {
+                2
+            } else {
+                0
+            }
+            putValue(status, control.first, control.second.iid)
+        }
+    }
+
+    private fun refreshMode(value: Int) {
+        binding.up.setBackgroundResource(R.drawable.bg_item)
+        binding.down.setBackgroundResource(R.drawable.bg_item)
+        if (value == 1) {
+            binding.up.setBackgroundResource(R.drawable.bg_item_blue)
+        } else if (value == 2) {
+            binding.down.setBackgroundResource(R.drawable.bg_item_blue)
+        }
+    }
+
+    override fun onValueChange(siid: Int, piid: Int, value: Any) {
+        value as Double
+        when (getPropertyName(piid)) {
+            "status" -> {
+                status = value.toInt()
+            }
+        }
+    }
+
+    override fun onValueChange(value: Any) = Unit
+
+}

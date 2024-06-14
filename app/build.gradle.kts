8c85fd6947038251daaf65eef2b046d668c198e0
Sky233ml <64515462+sky130@users.noreply.github.com>
2024-06-14 22:20:36 +0800

    更新版本号2.0.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.github.miwu"
         minSdk = 21
         targetSdk = 34
-        versionCode = 8
-        versionName = "2.0.6"
+        versionCode = 9
+        versionName = "2.0.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ksp {
             arg("room.schemaLocation", "$projectDir/schemas")

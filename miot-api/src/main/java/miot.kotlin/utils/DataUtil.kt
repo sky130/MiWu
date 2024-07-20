6d89b1b349c34b97218f3a0f9c9c303eb1edbfc3
Sky233ml <64515462+sky130@users.noreply.github.com>
2024-07-20 23:45:45 +0800

    修复方法命名错误

--- miot-api/src/main/java/miot.kotlin/utils/DataUtil.kt
+++ miot-api/src/main/java/miot.kotlin/utils/DataUtil.kt
@@ -13,15 +13,15 @@ import miot.kotlin.MiotManager.base64Encode
 
 fun generateSignedNonce(secret: String, nonce: String): String {
     val sha = MessageDigest.getInstance("SHA-256")
-    sha.update(base64Dcode(secret))
-    sha.update(base64Dcode(nonce))
+    sha.update(base64Decode(secret))
+    sha.update(base64Decode(nonce))
     return base64Encode(sha.digest())
 }
 
 fun generateSignature(uri: String, signedNonce: String, nonce: String, data: String): String {
     val sign = "$uri&$signedNonce&$nonce&data=$data".toByteArray(StandardCharsets.UTF_8)
     val mac = Mac.getInstance("HmacSHA256")
-    mac.init(SecretKeySpec(base64Dncode(signedNonce), "HmacSHA256"))
+    mac.init(SecretKeySpec(base64Decode(signedNonce), "HmacSHA256"))
     val digest = mac.doFinal(sign)
     return base64Encode(digest)
 }

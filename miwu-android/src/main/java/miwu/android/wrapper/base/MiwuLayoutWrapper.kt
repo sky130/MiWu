c1193fcd078752261b6029ee11ec6caeb3fb2673
Sky233 <sky233ml@qq.com>
2025-10-20 11:02:48 +0800

    feat: 支持 `MiwuLayoutWrapper`
    - 可以支持更多类型的设备

---
+++ miwu-android/src/main/java/miwu/android/wrapper/base/MiwuLayoutWrapper.kt
@@ -0,0 +1,27 @@
+package miwu.android.wrapper.base
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import androidx.viewbinding.ViewBinding
+import miwu.android.databinding.MiotLayoutWrapperBinding
+import miwu.support.base.MiwuWidget
+
+abstract class MiwuLayoutWrapper<T>(context: Context, widget: MiwuWidget<T>) :
+    BaseMiwuWrapper<T>(context, widget) {
+    private val binding by viewBinding(MiotLayoutWrapperBinding::inflate)
+    private val onClickView: View get() = view
+    override val view get() = binding.root
+
+    protected inline fun <reified VB : ViewBinding> view(
+        crossinline inflate: (LayoutInflater) -> VB,
+        block: VB.() -> Unit
+    ): VB = inflate(LayoutInflater.from(context)).apply {
+        block()
+        add(root)
+    }
+
+    fun add(content: View) {
+        binding.root.add(content)
+    }
+}

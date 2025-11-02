05f11e2b81538480483ce23588e9630f251b127f
Sky233 <sky233ml@qq.com>
2025-11-02 13:12:12 +0800

    Feat: 优化部分错误处理逻辑

--- miot-api/src/main/java/miwu/miot/exception/MiotAuthException.kt
+++ miot-api/src/main/java/miwu/miot/exception/MiotAuthException.kt
@@ -2,8 +2,13 @@ package miwu.miot.exception
 
 class MiotAuthException(message: String, cause: Throwable? = null) : MiotException(message, cause) {
     companion object {
-        fun tokenMissing(cause: Throwable? = null) = MiotAuthException("serviceToken or securityToken not found",cause)
-        fun tokenExpired(cause: Throwable? = null) = MiotAuthException("Token has expired",cause)
-        fun invalidCredentials(cause: Throwable? = null) = MiotAuthException("Invalid username or password",cause)
+        fun tokenMissing(cause: Throwable? = null) =
+            MiotAuthException("serviceToken or securityToken not found", cause)
+
+        fun tokenExpired(cause: Throwable? = null) =
+            MiotAuthException("Token has expired", cause)
+
+        fun invalidCredentials(cause: Throwable? = null) =
+            MiotAuthException("Invalid username or password", cause)
     }
 }

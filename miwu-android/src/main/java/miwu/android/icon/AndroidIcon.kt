4fd6b9db606e6c3c99020673b1af3be1805d0d17
Sky233 <sky233ml@qq.com>
2025-12-26 13:50:32 +0800

    Refactor: 优化 `RecyclerGridLayout` 布局和统一`列表`, `按钮`的背景颜色

--- miwu-android/src/main/java/miwu/android/icon/AndroidIcon.kt
+++ miwu-android/src/main/java/miwu/android/icon/AndroidIcon.kt
@@ -3,4 +3,4 @@ package miwu.android.icon
 import androidx.annotation.DrawableRes
 import miwu.support.icon.Icon
 
-data class AndroidIcon(@DrawableRes val resId : Int) : Icon
+data class AndroidIcon(@get:DrawableRes val resId: Int) : Icon
--- miwu-android/src/main/java/miwu/android/view/app/RecyclerGridLayout.kt
+++ miwu-android/src/main/java/miwu/android/view/app/RecyclerGridLayout.kt
@@ -23,7 +23,48 @@ class RecyclerGridLayout(context: Context, attr: AttributeSet) : RecyclerView(co
         setAdapter(adapter)
         setLayoutManager(layoutManager)
         setItemViewCacheSize(100)
-        addItemDecoration(SpacingItemDecoration(5))
+        isNestedScrollingEnabled = false
+        addItemDecoration(GridSpacingItemDecoration(spacing = 5, spanCount = 2))
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+        if (viewList.isEmpty()) {
+            return
+        }
+
+        val width = MeasureSpec.getSize(widthMeasureSpec)
+        val paddingLeft = paddingLeft
+        val paddingRight = paddingRight
+        val availableWidth = width - paddingLeft - paddingRight
+
+        var totalHeight = 0
+
+        for (i in viewList.indices) {
+            val view = viewList[i]
+            val column = i % 2
+
+            val childWidthSpec = MeasureSpec.makeMeasureSpec(
+                availableWidth / 2,
+                MeasureSpec.EXACTLY
+            )
+            val childHeightSpec = MeasureSpec.makeMeasureSpec(
+                0,
+                MeasureSpec.UNSPECIFIED
+            )
+
+            view.measure(childWidthSpec, childHeightSpec)
+
+            if (column == 0) {
+                totalHeight += view.measuredHeight + 5
+            }
+        }
+
+        setMeasuredDimension(
+            width,
+            totalHeight + paddingTop + paddingBottom
+        )
     }
 
     inner class GridViewHolder(val container: FrameLayout) : ViewHolder(container)
@@ -63,9 +104,73 @@ class RecyclerGridLayout(context: Context, attr: AttributeSet) : RecyclerView(co
         ) {
             super.getItemOffsets(outRect, view, parent, state)
             val spacingPx = dpToPx(spacing, parent.context)
-            outRect.left = spacingPx
-            outRect.right = spacingPx
             outRect.bottom = spacingPx
+        }
+
+        private fun dpToPx(dp: Int, context: Context): Int {
+            val density = context.resources.displayMetrics.density
+            return (dp * density).toInt()
+        }
+    }
+
+    class GridSpacingItemDecoration(
+        private val spacing: Int,
+        private val spanCount: Int = 2,
+        private val includeEdge: Boolean = false,
+        private val orientation: Int = GridLayoutManager.VERTICAL
+    ) : RecyclerView.ItemDecoration() {
+
+        override fun getItemOffsets(
+            outRect: Rect,
+            view: View,
+            parent: RecyclerView,
+            state: RecyclerView.State
+        ) {
+            super.getItemOffsets(outRect, view, parent, state)
+
+            val spacingPx = dpToPx(spacing, parent.context)
+            val position = parent.getChildAdapterPosition(view)
+
+            if (position == RecyclerView.NO_POSITION) return
+
+            val column = position % spanCount
+            val row = position / spanCount
+
+            if (orientation == GridLayoutManager.VERTICAL) {
+                if (includeEdge) {
+                    outRect.left = spacingPx - column * spacingPx / spanCount
+                    outRect.right = (column + 1) * spacingPx / spanCount
+
+//                    if (row == 0) {
+//                        outRect.top = spacingPx
+//                    }
+                    outRect.bottom = spacingPx
+                } else {
+                    outRect.left = column * spacingPx / spanCount
+                    outRect.right = spacingPx - (column + 1) * spacingPx / spanCount
+
+                    if (row != 0) {
+                        outRect.top = spacingPx
+                    }
+                }
+            } else {
+                if (includeEdge) {
+                    outRect.top = spacingPx - row * spacingPx / spanCount
+                    outRect.bottom = (row + 1) * spacingPx / spanCount
+
+                    if (column == 0) {
+                        outRect.left = spacingPx
+                    }
+                    outRect.right = spacingPx
+                } else {
+                    outRect.top = row * spacingPx / spanCount
+                    outRect.bottom = spacingPx - (row + 1) * spacingPx / spanCount
+
+                    if (column != 0) {
+                        outRect.left = spacingPx
+                    }
+                }
+            }
         }
 
         private fun dpToPx(dp: Int, context: Context): Int {
